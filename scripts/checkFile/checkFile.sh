cd523e7c607f5791e6c34ed16c52815c4fcee47f
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-05 00:42:01 -0500

    Continue...

--- scripts/checkFile/checkFile.sh
+++ scripts/checkFile/checkFile.sh
@@ -4,4 +4,6 @@ echo "### Cli touch"
 java -version
 ls -l $JENKINS_CLI_JAR_PATH
 echo $JENKINS_URL
-java -jar "$JENKINS_CLI_JAR_PATH" -s "$JENKINS_URL" help
+
+# java -jar "$JENKINS_CLI_JAR_PATH" -s "$JENKINS_URL" help
+java -jar "$JENKINS_CLI_JAR_PATH" help
