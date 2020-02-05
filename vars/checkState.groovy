9a515a104cea5c905797f2ed3aa2603a610e3eb4
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-05 01:13:35 -0500

    Continue...

--- vars/checkState.groovy
+++ vars/checkState.groovy
@@ -1,19 +1,18 @@
 def call() {
     String script = "${env.BASE_DIR}/scripts/checkFile/checkFile.sh"
     withEnv(["JENKINS_CLI_JAR_PATH=${env.BASE_DIR}/lib/jenkins-cli.jar"]){
-        executeScript(script)
+        // executeScript(script)
 
-/*         withCredentials([
+         withCredentials([
             usernamePassword(
                 usernameVariable: 'JENKINS_USER_ID',
                 passwordVariable: 'JENKINS_API_TOKEN',
                 credentialsId: 'continuous-deployment-test-token'
             )
         ]) {
-            
             executeScript(script)
         }
- */
+
     }
 
 }
