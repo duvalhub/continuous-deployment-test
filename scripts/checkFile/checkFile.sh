47646812bd86b416394f482ac6ee8d41aad51da2
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-05 22:51:22 -0500

    Continue...

--- scripts/checkFile/checkFile.sh
+++ scripts/checkFile/checkFile.sh
@@ -5,7 +5,4 @@ java -version
 ls -l $JENKINS_CLI_JAR_PATH
 echo $JENKINS_URL
 
-java -jar "$JENKINS_CLI_JAR_PATH" -s "$JENKINS_URL" version
-# java -jar "$JENKINS_CLI_JAR_PATH" version
-
-# java -jar "$JENKINS_CLI_JAR_PATH" help
+java -jar jenkins-cli.jar -s https://jenkins.philippeduval.ca -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s 'continuous-deployment/helloworld-app/test%2Fpipeline-development'
