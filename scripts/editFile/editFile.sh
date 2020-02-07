a52011b301a0434bb54e7f782f2e2d4f9fc1bf79
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-06 23:11:18 -0500

    Continue...

--- scripts/editFile/editFile.sh
+++ scripts/editFile/editFile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "allo" > toto
+file_to_edit="src/App.js"
+place_holder="#####"
+uuid=$(uuidgen)
+sed -i -E "s/$place_holder(.+)$place_holder/$place_holder$uuid$place_holder/" $file_to_edit
 git config --global user.email "toto-africa@email.com"
 git config --global user.name "Toto Africa"
-git add toto
+git add $file_to_edit
 git commit -m "Commit on toto"
 git push
