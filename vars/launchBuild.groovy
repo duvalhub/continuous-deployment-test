eaefcd667bc73a15565ddd3c018209f9630806de
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-10 00:55:13 -0500

    Continue...

--- vars/launchBuild.groovy
+++ vars/launchBuild.groovy
@@ -4,10 +4,7 @@ import com.duvalhub.jenkins.LaunchBuild
 def call(LaunchBuild launchBuild) {
     String script = "${env.WORKSPACE}/scripts/launchJenkins/launchJenkins.sh"
     withJenkins() {
-        withEnv([
-            "JENKINS_JOB=${launchBuild.getBuild}"
-        ]){
-            executeScript(script)
-        }
+        env.JENKINS_JOB = launchBuild.getBuild()
+        executeScript(script)
     }
 }
