2e79f679a3a6274fec9626f56d2415fd6a3332d8
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-10 01:18:14 -0500

    Continue...

--- scripts/launchJenkins/launchJenkins.sh
+++ scripts/launchJenkins/launchJenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 echo "### Lauching build job. JENKINS_CLI_JAR_PATH: '$JENKINS_CLI_JAR_PATH', JENKINS_URL: '$JENKINS_URL', JENKINS_PARAMS: '$JENKINS_PARAMS', JENKINS_JOB: '$JENKINS_JOB'"
-java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "$JENKINS_JOB" $JENKINS_JOB
+java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "$JENKINS_JOB" $( if [ ! -z $JENKINS_JOB ]; then printf $JENKINS_JOB; fi )
