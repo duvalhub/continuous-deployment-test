45485003a941e337684fd8ac03f6f36e5e65055a
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-10 00:14:20 -0500

    Continue...

--- scripts/launchJenkins/launchJenkins.sh
+++ scripts/launchJenkins/launchJenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "### Lauching build job"
-java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "continuous-deployment-tests/continuous-deployment-test-app/$APP_BRANCH"
+java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "continuous-deployment-pipelines/continuous-deployment-test-app/$APP_BRANCH"
 
 echo "### Looking for '$STRING_TO_SEARCH' at '$DEPLOYMENT_URL'"
 
