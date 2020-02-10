5405246c859ac50b4da83cdf9eb9eeaa014fc87b
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-10 01:02:06 -0500

    Continue...

--- vars/launchRelease.groovy
+++ vars/launchRelease.groovy
@@ -2,6 +2,6 @@ import com.duvalhub.continuousdeploymenttest.release.Release
 import com.duvalhub.jenkins.LaunchBuild
 
 def call (Release release) {
-    LaunchBuild launchBuild = new LaunchBuild("continuous-deployment-pipelines/continuous-deployment-release/feature%2Ffirst-draft")
-    launchBuild(launchBuild)
+    LaunchBuild launchBuildObject = new LaunchBuild("continuous-deployment-pipelines/continuous-deployment-release/feature%2Ffirst-draft")
+    launchBuild(launchBuildObject)
 }
