b4e1d70e8af248fa211de8c1f765b6765029f593
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-12 00:48:55 -0500

    Continue...

--- scripts/editFile/editFile.sh
+++ scripts/editFile/editFile.sh
@@ -2,8 +2,6 @@
 file_to_edit="$FILE_TO_EDIT"
 uuid="$UUID"
 echo -n "$uuid" > $file_to_edit
-git config --global user.email "toto-africa@email.com"
-git config --global user.name "Toto Africa"
 git add $file_to_edit
 git commit -m "Edited '$file_to_edit' with uuid '$uuid'"
 git push
