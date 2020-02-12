caebcb0318bd8932fce7e1d8fa2adde60f1d3e1b
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-12 02:25:07 -0500

    Continue...

--- vars/determineReleaseBranch.groovy
+++ vars/determineReleaseBranch.groovy
@@ -2,7 +2,7 @@ import com.duvalhub.continuousdeploymenttest.trace.Trace
 def call(Trace trace) {
     withSshKey() {
         dir( trace.app_workdir ) {
-            String script = "${env.PIPELINE_WORKDIR}/scripts/getReleaseBranch/getReleaseBranch.sh"
+            String script = "${env.PIPELINE_WORKDIR}/scripts/bash/getReleaseBranch/getReleaseBranch.sh"
             return executeScript(script, true)
         }
     }
