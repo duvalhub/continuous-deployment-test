d2b77337af2f51062dd1b683329f99430da71300
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-15 19:45:23 -0500

    Continue...

--- scripts/launchJenkins/launchJenkins.sh
+++ scripts/launchJenkins/launchJenkins.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 echo "### Lauching build job. JENKINS_CLI_JAR_PATH: '$JENKINS_CLI_JAR_PATH', JENKINS_URL: '$JENKINS_URL', JENKINS_PARAMS: '$JENKINS_PARAMS', JENKINS_JOB: '$JENKINS_JOB'"
 
 attempt_counter=0
@@ -15,17 +14,17 @@ do
     fi
 
     echo "Launching job '$JENKINS_JOB'"
-    job_success=false
-    java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "$JENKINS_JOB" $( if [ ! -z "$JENKINS_PARAMS" ]; then echo -n "$JENKINS_PARAMS"; fi ) && job_success=true || job_success=false
-
-    if [ "$job_success" = true ]; then
-        echo "Job success."
-        exit 0
+    set +e
+    java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "$JENKINS_JOB" $( if [ ! -z "$JENKINS_PARAMS" ]; then echo -n "$JENKINS_PARAMS"; fi ) 
+    build_error_code=$(echo $?)
+    set -e
+    if (( $build_error_code != 3)); then
+      echo "Job failed. Crash..."; 
+      exit 1
+    else 
+      echo "whou"; 
+      echo "Job not found. Tried #$((attempt_counter+1))/$max_attempts . Sleeping for $sleep_time second."
+      attempt_counter=$(($attempt_counter+1))
+      sleep $sleep_time
     fi
-
-    echo "Job failed. Tried #$((attempt_counter+1))/$max_attempts . Sleeping for $sleep_time second."
-
-    attempt_counter=$(($attempt_counter+1))
-    sleep $sleep_time
 done
-
