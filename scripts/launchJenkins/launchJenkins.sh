ed55b9955577d871ffcf1f9f9af88b5b77d79f4c
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-16 18:12:28 -0500

    Continue...

--- scripts/launchJenkins/launchJenkins.sh
+++ scripts/launchJenkins/launchJenkins.sh
@@ -13,12 +13,13 @@ while true; do
   fi
 
   set +e
-  curl "$JENKINS_URL/$JENKINS_JOB_HTTP/api/json"
+  curl -u "$JENKINS_USER_ID:$JENKINS_API_TOKEN" "$JENKINS_URL/$JENKINS_JOB_HTTP/api/json"
   build_error_code=$(echo $?)
   set -e
   if (( $build_error_code == 0)); then
     echo "Launching job '$JENKINS_JOB'"
-    java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "$JENKINS_JOB" $( if [ ! -z "$JENKINS_PARAMS" ]; then echo -n "$JENKINS_PARAMS"; fi ) 
+    java -jar $JENKINS_CLI_JAR_PATH -s $JENKINS_URL -webSocket -auth "$JENKINS_USER_ID:$JENKINS_API_TOKEN" build -s "$JENKINS_JOB" $( if [ ! -z "$JENKINS_PARAMS" ]; then echo -n "$JENKINS_PARAMS"; fi )
+    exit 0
   else 
     echo "Job not found. Tried #$((attempt_counter+1))/$max_attempts . Sleeping for $sleep_time second."
     attempt_counter=$(($attempt_counter+1))
