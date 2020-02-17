3ce4d4221ec996ce01461b017fcb4dc644b6fd2e
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-16 20:18:10 -0500

    Continue...

--- vars/wut.groovy
+++ vars/wut.groovy
@@ -1,8 +1,8 @@
+echo "wut wut ?"
 import com.duvalhub.continuousdeploymenttest.trace.Trace
 @Library('shared-library')
 import com.duvalhub.appconfig.Platforms
 
 def call () {
-    library "shared-library@${env.PIPELINE_BRANCH}"
     echo "uh"
 }
